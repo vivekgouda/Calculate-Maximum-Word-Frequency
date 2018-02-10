b73ef1f5627f45612ce4bd3bcf078e65823f384e
vivekgouda <vivekgouda2@gmail.com>
2018-02-11 00:22:05 +0530

    Add all my files

---
+++ my_sol/solution.rb
@@ -0,0 +1,7 @@
+require_relative "module2_assignment"
+
+solution = Solution.new
+#expect errors until you implement these methods
+solution.analyze_file
+solution.calculate_line_with_highest_frequency
+solution.print_highest_word_frequency_across_lines
